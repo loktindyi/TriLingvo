79a72fb47b0fa30011712d6a074f6b912c143623
言叶与言 <67369578+loktindyi@users.noreply.github.com>
2020-09-15 11:44:52 +0800

    Update Tri-UserCss-latest.user.js

--- Tri-UserCss-latest.user.js
+++ Tri-UserCss-latest.user.js
@@ -4,7 +4,7 @@
 // @name:zh      Tri-言叶言语
 // @name:en      TriLingvo
 // @namespace    https://space.bilibili.com/379335206
-// @version      1.60
+// @version      1.62
 // @description:zh 自定义任何网站的样式 反馈群：884813590
 // @description:en Customize css style of any site QQGroup:884813590
 // @author       哔哩哔哩@言叶与言
@@ -13,13 +13,14 @@
 // @supportURL   https://jq.qq.com/?_wv=1027&k=IMqY916N
 // @updateURL    https://cdn.jsdelivr.net/gh/loktindyi/userCss@master/Tri-UserCss-latest.user.js
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.4.0/dist/jquery.min.js
-// @icon         https://cdn.jsdelivr.net/gh/loktindyi/mybaiduhp@master/Tri.png
+// @icon         https://cdn.jsdelivr.net/gh/loktindyi/userCss@master/Tri.png
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_listValues
 // @grant        GM_deleteValue
 // @grant        GM_registerMenuCommand
+// @note         1.62 修复泛匹配错误 更新了变量名|未来计划：开发者模式——更多信息
 // @note         1.60 改名
 // @note         1.50 移除正则 改用关键词匹配
 // @note         1.40 支持引用样式 多个正则可以引用同一样式 |支持纯字符串匹配
@@ -44,45 +45,44 @@
 			TriLingvoEstasEll: 1,
 			Prompto: "绝对匹配|这个例子指定仅对哔哩哔哩主页注入样式|被引用时需要Trindex",
 			Trindex: 0,
-			TriLingvo: "www.bilibili.com",
-			TriUserCss: "body{background-size: cover; background-attachment: fixed; background-image: url('https://g.hiphotos.baidu.com/zhidao/pic/item/8644ebf81a4c510f973523a36b59252dd52aa592.jpg')}"
+			TriEsprimo: "www.bilibili.com",
+			TriLingvo: ".simplify-home{background-size: cover; background-attachment: fixed; background-image: url('https://g.hiphotos.baidu.com/zhidao/pic/item/8644ebf81a4c510f973523a36b59252dd52aa592.jpg')}"
 		},
 		{
 			Prompto: "泛匹配|引用样式",
 			Trindex: 1,
-			TriLingvo: "www.bilibili.com/video/*",
-			TriUserCss: "0"
+			TriEsprimo: "bilibili.com/video/*",
+			TriLingvo: "0"
 		},
 		{
 			Prompto: "TrindexIsNotNecessary >> Tri",
-			TriLingvo: "",
-			TriUserCss: ""
+			TriEsprimo: "",
+			TriLingvo: ""
 		}
 	]
 	//关键词匹配
-	function Lingvo(_TriLingvo){
-		let LingvoAl = _TriLingvo.indexOf("*");
-		if (LingvoAl != -1){
-			let Vzin = 0;
-			_TriLingvo = _TriLingvo.split("*");
-			for (let i = 0; i < _TriLingvo.length; i++){if(window.location.href.indexOf(_TriLingvo[i]) != -1){Vzin++}}
-			if (Vzin = _TriLingvo.length){return true}else{return false}
+	function Lingvo(_TriEsprimo){
+		if (_TriEsprimo.indexOf("*") != -1){
+			let v =0;
+			_TriEsprimo = _TriEsprimo.split("*");
+			for (let i = 0; i < _TriEsprimo.length; i++){if(window.location.href.indexOf(_TriEsprimo[i]) != -1){v++}}
+			if (v == _TriEsprimo.length){console.log(_TriEsprimo);return true}else{return false}
 		}else{
 			let curl = window.location.href.replace(/https?:\/\//,"");
 			if (curl[curl.length-1] == "/"){curl = curl.slice(0,curl.length-1)}
-			if (_TriLingvo == curl){return true}else{return false}
+			if (_TriEsprimo == curl){console.log(_TriEsprimo+curl);return true}else{return false}
 		}
 	}
 	//取得用户样式 取得索引 加载
 	VzinConfig = GM_getValue("VzinConfig",VzinConfig);
 	try{
 		for (let tri = 0; tri < VzinConfig.length; tri++){
-			if (Lingvo(VzinConfig[tri].TriLingvo)){
+			if (Lingvo(VzinConfig[tri].TriEsprimo)){
 				var Trindex = tri,_Trindex;
-				var TriCss = VzinConfig[tri].TriUserCss;
+				var TriCss = VzinConfig[tri].TriLingvo;
 				if (/^\d{1}$/.test(TriCss)){
 					_Trindex = parseInt(TriCss);
-					TriCss = VzinConfig[_Trindex].TriUserCss;
+					TriCss = VzinConfig[_Trindex].TriLingvo;
 					console.log(tri + " >> 引用样式 => 索引为 " + _Trindex);
 					_Trindex = " >> " + _Trindex
 				}else{_Trindex = ""}
@@ -95,10 +95,10 @@
 	GM_registerMenuCommand("调试",function(){
 		if (!$("#Vzin_user_btn")[0]){$("body")[0].innerHTML += `
 <input id="Vzin-joinus" type="button" style="display:none" class="Vzin-joinus" onclick="window.open('https://jq.qq.com/?_wv=1027&k=IMqY916N')" value="加入星凰">
-<input id="Vzin_user_btn" type="button" class="Vzin_user_btn" onclick="if (document.getElementById('Vzin_container').style.display == 'none'){document.getElementById('Vzin_container').style.display = 'block'}else{document.getElementById('Vzin_container').style.display = 'none'}">
+<input id="Vzin_user_btn" type="button" class="Vzin_user_btn" onclick="if (document.querySelector('#Vzin_container').style.display == 'none'){document.querySelector('#Vzin_container').style.display = 'block'}else{document..querySelector('#Vzin_container').style.display = 'none'}">
 <div id="Vzin_container" style="display: block">
 <div class="Trindex">La nuna informoj: <font color="#fa7298">` + Trindex + _Trindex + `</font></div>
-<textarea id="Vzin_user_config" class="Vzin_user_config" onfocus="document.querySelector('#Vzin-joinus').style.display= 'block'" onkeyup="document.getElementById('Vzin_user_style').innerHTML = JSON.parse(this.value)[` + Trindex + `].TriUserCss" type="input">` + JSON.stringify(VzinConfig).replace(/,/g,",\n").replace(/\},/g,"},\n").replace(/Ell":1,/g,'Ell":1,\n') + `</textarea>
+<textarea id="Vzin_user_config" class="Vzin_user_config" onfocus="document.querySelector('#Vzin-joinus').style.display= 'block'" onkeyup="document.querySelector('#Vzin_user_style').innerHTML = JSON.parse(this.value)[` + Trindex + `].TriLingvo" type="input">` + JSON.stringify(VzinConfig).replace(/,/g,",\n").replace(/\},/g,"},\n").replace(/Ell":1,/g,'Ell":1,\n') + `</textarea>
 </div><style id="Vzin_user_style" type="text/css">` + TriCss + `</style>`
 		}});
 	GM_registerMenuCommand("保存",function(){GM_setValue("VzinConfig",JSON.parse($("#Vzin_user_config")[0].value.replace(/\n/g,"")));setTimeout(function(){window.location.reload()}, 200)});
